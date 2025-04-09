5ae2b614825790a4d6f655b818bcbf9359ce73b9
John Reardon <40861351+froazin@users.noreply.github.com>
2025-04-09 20:48:44 +0000

    refactor(devcontainer): remove sdk dep

--- .devcontainer/features/utils/install.sh
+++ .devcontainer/features/utils/install.sh
@@ -2,45 +2,40 @@
 
 set -e
 
-eval "$(sdkmod logging)" || exit 1
-eval "$(sdkmod common)" || exit 1
-
-_FEATURE_NAME="utils"
-
 function main {
-    log info "Setting up utils."
+    echo "Setting up utils."
 
     for file in "$(dirname "$0")/bin/"*; do
         local file_name
         file_name=$(basename "$file")
 
         if [ -z "$file_name" ]; then
-            log error "File name is empty. Skipping."
+            echo "File name is empty. Skipping."
             continue
         fi
 
         cp --force "$file" "/usr/local/bin/$file_name" || {
-            log error "Failed to copy $file to /usr/local/bin/$file_name."
+            echo "Failed to copy $file to /usr/local/bin/$file_name."
             return 1
         }
 
         chmod +x "/usr/local/bin/$file_name" || {
-            log error "Failed to make $file executable."
+            echo "Failed to make $file executable."
             return 1
         }
 
-        log info "Copied $file to /usr/local/bin/$file_name."
+        echo "Copied $file to /usr/local/bin/$file_name."
     done
 
-    check_commands generate-docs || {
-        log error "generate-docs did not install correctly."
+    if ! type generate-docs >/dev/null 2>&1; then
+        echo "generate-docs did not install correctly."
         return 1
-    }
+    fi
 
-    log info "Done."
+    echo "Done."
     return 0
 }
 
 main "$@" || {
-    log fatal "Setup failed for $? package(s)."
+    echo "Setup failed for $? package(s)."
 }
